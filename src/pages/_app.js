1069ba36b0f066228bb3bdb36971c2a45dd6f627
Your NameMoCheeseKy <rifkyking04@gmail.com>
2025-09-30 21:17:37 +0700

    .

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,25 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { Toaster } from '@/components/ui/toaster';
 import '@/styles/globals.css';
 
 function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem('cyclefy_user_token');
+
+    const protectedRoutes = ['/user', '/features'];
+
+    const needsAuth = protectedRoutes.some((path) =>
+      router.pathname.startsWith(path)
+    );
+
+    if (needsAuth && !token) {
+      router.replace('/login');
+    }
+  }, [router]);
+
   return (
     <>
       <Component {...pageProps} />
